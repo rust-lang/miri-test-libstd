0663bad66f2cf91b8845a95d5e8263724a523ecc
Ralf Jung <post@ralfj.de>
2019-02-08 15:09:49 +0100

    travis: apply a patch to the rust-src

--- travis.sh
+++ travis.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+export RUST_SRC=$(rustc --print sysroot)/lib/rustlib/src/rust/src
+
+# apply our patch
+PATCH=$(readlink -e rust-src.diff)
+{ cd $RUST_SRC && patch -p1 < $PATCH }
+
+# run the tests
 ./run-test.sh core
 ./run-test.sh alloc
