5657ea3e81290e1e080ec5b23ef6b66b6f09dfe7
Ralf Jung <post@ralfj.de>
2019-06-11 09:51:45 +0200

    also test without validation

--- travis.sh
+++ travis.sh
@@ -6,7 +6,8 @@ rm -rf rust-src-patched
 cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 ( cd rust-src-patched && patch -f -p1 < ../rust-src.diff )
 
-# run the tests
+# run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
+MIRI_EXTRA_FLAGS="-Zmiri-disable-validation" ./run-test.sh core
 ./run-test.sh core
 ./run-test.sh alloc
