f7dad734d8f27e52aeae62c98e13e401de2143a1
Ralf Jung <post@ralfj.de>
2020-03-24 08:07:26 +0100

    bump nightly; the leaks should be fixed now

--- travis.sh
+++ travis.sh
@@ -9,8 +9,8 @@ cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
 echo && echo "## Testing core (without validation and Stacked Borrows)" && echo
-./run-test.sh core -Zmiri-ignore-leaks -Zmiri-disable-validation -Zmiri-disable-stacked-borrows
+./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows
 echo && echo "## Testing core" && echo
-./run-test.sh core -Zmiri-ignore-leaks
+./run-test.sh core
 echo && echo "## Testing alloc" && echo
 ./run-test.sh alloc
