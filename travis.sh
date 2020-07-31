ad1ae01ec89c542476f687ba6d788a9cb2abf2ae
Ralf Jung <post@ralfj.de>
2020-07-31 10:33:17 +0200

    also take test times on CI

--- travis.sh
+++ travis.sh
@@ -9,8 +9,8 @@ cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
 echo && echo "## Testing core (without validation and Stacked Borrows)" && echo
-./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows
+./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows 2>&1 | ts -m -i '%.s  '
 echo && echo "## Testing core" && echo
-./run-test.sh core
+./run-test.sh core 2>&1 | ts -m -i '%.s  '
 echo && echo "## Testing alloc" && echo
-./run-test.sh alloc
+./run-test.sh alloc 2>&1 | ts -m -i '%.s  '
