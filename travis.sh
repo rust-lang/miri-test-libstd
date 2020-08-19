3705046210c98301cba5c8ed581928fd8591f3b9
Ralf Jung <post@ralfj.de>
2020-08-19 09:50:16 +0200

    bump Rust; also test with symbolic alignment

--- travis.sh
+++ travis.sh
@@ -8,9 +8,9 @@ cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
-echo && echo "## Testing core (without validation and Stacked Borrows)" && echo
-./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows 2>&1 | ts -i '%.s  '
+echo && echo "## Testing core (no validation, no Stacked Borrows, symbolic alignment)" && echo
+./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check -- --skip align 2>&1 | ts -i '%.s  '
 echo && echo "## Testing core" && echo
 ./run-test.sh core 2>&1 | ts -i '%.s  '
-echo && echo "## Testing alloc" && echo
-./run-test.sh alloc 2>&1 | ts -i '%.s  '
+echo && echo "## Testing alloc (symbolic alignment)" && echo
+./run-test.sh alloc -Zmiri-symbolic-alignment-check 2>&1 | ts -i '%.s  '
