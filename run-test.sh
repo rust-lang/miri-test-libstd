a0719e6ca40f2772239c291699acd61dcda013ce
Ralf Jung <post@ralfj.de>
2020-09-21 09:14:54 +0200

    update Miri argument passing

--- run-test.sh
+++ run-test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ## Run a Rust libstd test suite with Miri.
 ## Assumes Miri to be installed.
 ## Usage:
-##   ./run-test.sh CRATE_NAME
+##   ./run-test.sh CRATE_NAME CARGO_TEST_ARGS
 ## Environment variables:
 ##   RUST_SRC: The path to the Rust source directory (containing `src`).
 ##     Defaults to `$(rustc --print sysroot)/lib/rustlib/src/rust` or
@@ -35,4 +35,4 @@ ln -s "$RUST_SRC"/library/$CRATE lib$CRATE
 # run test
 cd ${CRATE}_miri_test
 XARGO_RUST_SRC="$RUST_SRC/library" cargo miri setup
-MIRI_SYSROOT=~/.cache/miri/HOST cargo miri test -- "$@"
+MIRI_SYSROOT=~/.cache/miri/HOST cargo miri test "$@"
--- travis.sh
+++ travis.sh
@@ -9,8 +9,8 @@ cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
 echo && echo "## Testing core (no validation, no Stacked Borrows, symbolic alignment)" && echo
-./run-test.sh core -Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check -- --skip align 2>&1 | ts -i '%.s  '
+MIRIFLAGS="-Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check" ./run-test.sh core -- --skip align 2>&1 | ts -i '%.s  '
 echo && echo "## Testing core" && echo
 ./run-test.sh core 2>&1 | ts -i '%.s  '
 echo && echo "## Testing alloc (symbolic alignment)" && echo
-./run-test.sh alloc -Zmiri-symbolic-alignment-check 2>&1 | ts -i '%.s  '
+MIRIFLAGS="-Zmiri-symbolic-alignment-check" ./run-test.sh alloc 2>&1 | ts -i '%.s  '
