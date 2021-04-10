7bb50aebc80c4b29deb42dac726533c275ce5287
Ralf Jung <post@ralfj.de>
2021-04-10 12:30:55 +0200

    run doctests (and add required patch)

--- travis.sh
+++ travis.sh
@@ -8,9 +8,22 @@ cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
+
+# libcore
 echo && echo "## Testing core (no validation, no Stacked Borrows, symbolic alignment)" && echo
-MIRIFLAGS="-Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check" ./run-test.sh core --lib --tests -- --skip align 2>&1 | ts -i '%.s  '
+MIRIFLAGS="-Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check" \
+  ./run-test.sh core --all-targets -- --skip align 2>&1 | ts -i '%.s  '
 echo && echo "## Testing core" && echo
-./run-test.sh core --lib --tests 2>&1 | ts -i '%.s  '
+MIRIFLAGS="" \
+  ./run-test.sh core --all-targets 2>&1 | ts -i '%.s  '
+echo && echo "## Testing core (doctests)" && echo
+MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation" \
+  ./run-test.sh core --doc
+
+# liballoc
 echo && echo "## Testing alloc (symbolic alignment)" && echo
-MIRIFLAGS="-Zmiri-symbolic-alignment-check" ./run-test.sh alloc --lib --tests 2>&1 | ts -i '%.s  '
+MIRIFLAGS="-Zmiri-symbolic-alignment-check" \
+  ./run-test.sh alloc --all-targets 2>&1 | ts -i '%.s  '
+echo && echo "## Testing alloc (doctests)" && echo
+MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation" \
+  ./run-test.sh alloc --doc
