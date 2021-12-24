4ab321837dbbdf42fceb118e3254e84781b2b0cf
Ralf Jung <post@ralfj.de>
2021-12-24 20:17:59 +0100

    adjust for MIRI_LIB_SRC env var

--- run-test.sh
+++ run-test.sh
@@ -7,10 +7,8 @@ set -euo pipefail
 ## Usage:
 ##   ./run-test.sh CRATE_NAME CARGO_TEST_ARGS
 ## Environment variables:
-##   RUST_SRC: The path to the Rust source directory (containing `src`).
-##     Defaults to `$(rustc --print sysroot)/lib/rustlib/src/rust` or
-##     `$(rustc --print sysroot)/../../..`, which ever exists.  (The former
-##     works for distributed toolchains, the latter for locally built ones.)
+##   MIRI_LIB_SRC: The path to the Rust library directory (`library`).
+##     Defaults to `$(rustc --print sysroot)/lib/rustlib/src/rust/library`.
 
 CRATE=${1:-}
 if [[ -z "$CRATE" ]]; then
@@ -19,21 +17,22 @@ if [[ -z "$CRATE" ]]; then
 fi
 shift
 
-DEFAULT_RUST_SRC=$(rustc --print sysroot)/lib/rustlib/src/rust
-RUST_SRC=${RUST_SRC:-$DEFAULT_RUST_SRC}
-if ! test -f "$RUST_SRC/Cargo.lock"; then
-    echo "Rust source dir ($RUST_SRC) does not contain a Cargo.lock file."
-    echo "Set RUST_SRC to the Rust source directory, or install the rust-src component."
+# compute the library directory (and export for Miri)
+MIRI_LIB_SRC=${MIRI_LIB_SRC:-$(rustc --print sysroot)/lib/rustlib/src/rust/library}
+if ! test -d "$MIRI_LIB_SRC/core"; then
+    echo "Rust source dir ($MIRI_LIB_SRC) does not contain a 'core' subdirectory."
+    echo "Set MIRI_LIB_SRC to the Rust source directory, or install the rust-src component."
     exit 1
 fi
 # macOS does not have a useful readlink/realpath so we have to use Python instead...
-RUST_SRC=$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$RUST_SRC")
+MIRI_LIB_SRC=$(python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$MIRI_LIB_SRC")
+export MIRI_LIB_SRC
 
 # update symlink
 rm -f lib$CRATE
-ln -s "$RUST_SRC"/library/$CRATE lib$CRATE
+ln -s "$MIRI_LIB_SRC"/$CRATE lib$CRATE
 
 # run test
 cd ${CRATE}_miri_test
-XARGO_RUST_SRC="$RUST_SRC/library" cargo miri setup
-MIRI_SYSROOT=~/.cache/miri/HOST cargo miri test "$@"
+cargo miri setup
+cargo miri test "$@"
