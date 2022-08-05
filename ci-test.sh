3de85b74fc904eb9b4992adc47a99a6bf691057f
Ralf Jung <post@ralfj.de>
2022-08-05 18:52:16 -0400

    run more std tests

--- ci-test.sh
+++ ci-test.sh
@@ -37,7 +37,7 @@ alloc)
     ;;
 std)
     # Only test a few things here with permissive flags, we are still testing the waters.
-    MODULES="env:: ffi:: buffered:: mpsc:: thread::"
+    MODULES="env:: ffi:: buffered:: mpsc:: thread:: error:: collections::"
     echo && echo "## Testing std ($MODULES)" && echo
     MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-permissive-provenance" \
         ./run-test.sh std --lib --tests \
