946128b43bd01fc0441f4e1df6efe1df7e5fddc7
Ralf Jung <post@ralfj.de>
2022-08-05 20:51:16 -0400

    split std tests into two batches

--- ci-test.sh
+++ ci-test.sh
@@ -40,16 +40,22 @@ std)
     # - f32, f64: use some unsupported float operations
     # - fs, net, process, sys, sys_common::net: need a lot of shims we don't support
     # - io::error: needs https://github.com/rust-lang/miri/pull/2465
-    # - FIXME: sync::mutex: fails, but only on CI (?!?)
     # Additionally we skip some of the integration tests:
     # - env_home_dir: needs a shim we don't support
     # - sleep: needs https://github.com/rust-lang/miri/pull/2466
     SKIP="f32:: f64:: fs:: net:: process:: sys:: sys_common::net:: io::error:: sync::mutex:: env_home_dir sleep"
     # hashbrown does int2ptr casts, so we need permissive provenance.
+    # We run the tests in two batches (`sync::` and the rest), as somehow it fails on CI otherwise (are we using too much RAM?).
     echo && echo "## Testing std (except for $SKIP)" && echo
     MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-permissive-provenance" \
         ./run-test.sh std --lib --tests \
-        -- $(for M in $SKIP; do echo "--skip $M "; done) \
+        -- sync:: \
+        2>&1 | ts -i '%.s  '
+    echo && echo "## Testing std (except for $SKIP)" && echo
+    MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-permissive-provenance" \
+        ./run-test.sh std --lib --tests \
+        -- --skip sync:: \
+        $(for M in $SKIP; do echo "--skip $M "; done) \
         2>&1 | ts -i '%.s  '
     echo && echo "## Testing std docs (except for $SKIP)" && echo
     MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation -Zmiri-permissive-provenance" \
