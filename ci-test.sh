4d70970d902b03289ed193df9f5860c6fc025222
Ralf Jung <post@ralfj.de>
2022-08-22 09:37:55 -0400

    print an error when applying the patch failed

--- ci-test.sh
+++ ci-test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # apply our patch
 rm -rf rust-src-patched
 cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
-( cd rust-src-patched && patch -f -p1 < ../rust-src.diff >/dev/null )
+( cd rust-src-patched && patch -f -p1 < ../rust-src.diff >/dev/null ) || ( echo "Applying rust-src.diff failed!" && exit 1 )
 export MIRI_LIB_SRC=$(pwd)/rust-src-patched/library
 
 # run the tests (some also without validation, to exercise those code paths in Miri)
