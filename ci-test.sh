8ca7d0e68726198d58c926bf19fff26425d5c4bf
Ralf Jung <post@ralfj.de>
2023-08-05 08:01:15 +0200

    need to allow internal_features

--- ci-test.sh
+++ ci-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-DEFAULTFLAGS="-Zmiri-retag-fields -Zrandomize-layout -Zmiri-strict-provenance"
+DEFAULTFLAGS="-Zmiri-retag-fields -Zrandomize-layout -Zmiri-strict-provenance -Ainternal_features"
 
 # apply our patch
 rm -rf rust-src-patched
