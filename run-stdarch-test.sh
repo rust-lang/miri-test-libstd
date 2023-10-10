48e317c48e7af5268d12e159c6b8451b49e146e3
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2023-10-10 20:55:46 +0200

    Remove no longer needed skips for stdarch

--- run-stdarch-test.sh
+++ run-stdarch-test.sh
@@ -23,27 +23,8 @@ i586-*|i686-*|x86_64-*)
     TEST_ARGS=(
         core_arch::x86::{sse,sse2,sse3,ssse3}::
         core_arch::x86_64::{sse,sse2}::
-        # FIXME add `#[cfg_attr(miri, ignore)]` to those tests in stdarch
-        # These are nontemporal stores, fences, and CSR (FP env status register) accesses
-        --skip test_mm_comieq_ss_vs_ucomieq_ss
-        --skip test_mm_getcsr_setcsr_1
-        --skip test_mm_getcsr_setcsr_2
-        --skip test_mm_getcsr_setcsr_underflow
-        --skip test_mm_sfence
-        --skip test_mm_stream_ps
-        --skip test_mm_clflush
-        --skip test_mm_lfence
-        --skip test_mm_maskmoveu_si128
-        --skip test_mm_mfence
-        --skip test_mm_stream_pd
-        --skip test_mm_stream_si128
-        --skip test_mm_stream_si32
-        --skip test_mm_stream_si64
-        # FIXME fix those in stdarch
-        --skip test_mm_rcp_ss # __m128(0.24997461, 13.0, 16.0, 100.0) != __m128(0.24993896, 13.0, 16.0, 100.0)
-        --skip test_mm_store1_ps # attempt to subtract with overflow
-        --skip test_mm_store_ps # attempt to subtract with overflow
-        --skip test_mm_storer_ps # attempt to subtract with overflow
+        # FIXME not yet implemented
+        --skip test_mm_clflush # could be implemented as a no-op?
     )
     ;;
 *)
